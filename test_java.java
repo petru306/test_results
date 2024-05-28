04d3a41abcbcf5f734f7737828c6bd5f24ebe277
petru306 <152027657+petru306@users.noreply.github.com>
2024-05-28 19:09:22 +0200

    Add files via upload

---
+++ test_java.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class test_java {
+
+    @Test
+    void test_java(){
+        int result = 892 - 456 / 2 + 10;
+        System.out.println("Result: " + result);
+
+    }
+
+
+}
